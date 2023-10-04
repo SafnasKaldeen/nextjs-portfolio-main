0e684d04f1f6133889020f3ebb860b616bc08b72
SafnasKaldeen <110978617+SafnasKaldeen@users.noreply.github.com>
2023-10-04 08:39:04 +0530

    module updated

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -4,7 +4,6 @@ const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
-    console.log(process.env.PROJECTS_COUNT);
   } catch (error) {
     console.log("Could not connect to MongoDB");
   }
