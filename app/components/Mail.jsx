47d1ee5607055e260eec7267197ad309111ee26b
SafnasKaldeen <110978617+SafnasKaldeen@users.noreply.github.com>
2024-08-03 15:25:58 +0530

    updated CV

--- app/components/Mail.jsx
+++ app/components/Mail.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useState } from "react";
 
 import { useForm, ValidationError } from "@formspree/react";
--- app/loading.jsx
+++ app/loading.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="loading">
+      <div className="loading-spinner"></div>
       <p>Loading...</p>
     </div>
   );
